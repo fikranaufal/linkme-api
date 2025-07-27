b7ecd8f2e089f246e65de8ceb95c2306aadfec03
fikranaufal <fikran.naufal@gmail.com>
2025-07-27 14:11:33 +0700

    Adding some features in usersController

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import prisma from "../lib/prisma"
+import prisma from "../lib/prisma";
+import bcrypt from "bcrypt";
 
 class UsersController {
     async getAllUsers(req: Request, res: Response) {
@@ -25,12 +26,14 @@ class UsersController {
     async createUser(req: Request, res : Response) {
         try {
             const { name, email, username, password } = req.body;
+
+            const hashedPassword = bcrypt.hashSync(password,10)
             const user = await prisma.user.create({
                 data: {
                     name,
                     email,
                     username,
-                    password,
+                    password: hashedPassword,
                 },
             });
 
@@ -45,6 +48,97 @@ class UsersController {
                 status: "error",
                 code: 500,
                 message: "Failed to create user",
+                error: error,
+            });
+        }
+    }
+
+    async detailUser(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+            const user = await prisma.user.findUnique({
+                where: {
+                    id,
+                }
+            });
+
+            if (!user) {
+                throw new Error("No user found");
+            }
+
+            res.json({
+                status: "ok",
+                code: 200,
+                message: "Users fetched succesfully",
+                data: user,
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "error",
+                code: 500,
+                message: "Failed to get users",
+                error: error,
+            });
+        }
+    }
+
+    async updateUser(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+            const updatedUser = req.body;
+
+            if (updatedUser.password){
+                const hashedPassword = bcrypt.hashSync(updatedUser.password,10);
+                updatedUser.password = hashedPassword;
+            }
+            
+            const user = await prisma.user.update({
+                where: {
+                    id
+                },
+                data: updatedUser,
+            });
+
+            res.json({
+                status: "ok",
+                code: 201,
+                message: "Updated successfully",
+                data: user,
+            })
+        } catch (error) {
+            res.status(500).json({
+                status: "error",
+                code: 500,
+                message: "Failed to updated user",
+                error: error,
+            });
+        }
+    }
+
+    async deleteUser(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+            const user = await prisma.user.delete({
+                where: {
+                    id,
+                }
+            });
+
+            if (!user) {
+                throw new Error("No user found");
+            }
+
+            res.json({
+                status: "ok",
+                code: 200,
+                message: "Users deleted succesfully",
+                data: user,
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "error",
+                code: 500,
+                message: "Failed to delete users",
                 error: error,
             });
         }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,5 +4,8 @@ import UsersController from "../controllers/UsersController";
 const router = Router();
 router.get("/", UsersController.getAllUsers);
 router.post("/", UsersController.createUser);
+router.get("/:id",UsersController.detailUser);
+router.put("/:id", UsersController.updateUser);
+router.delete("/:id",UsersController.deleteUser);
 
 export default router;
